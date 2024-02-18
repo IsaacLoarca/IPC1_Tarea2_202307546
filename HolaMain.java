9e4efe10fde02f22ef876c0dad47ba311620f9ae
Isaac Mahanaim Loarca Bautista <160090185+IsaacLoarca@users.noreply.github.com>
2024-02-17 21:00:23 -0600

    Update HolaMain.java

--- HolaMain.java
+++ HolaMain.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package holamain;
 
-/**
- *
- * @author ByHaisack
- */
 public class HolaMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola desde main");
     }
